539ec39c1a4148ab80c688b3b5a809dbe879f36d
James Fulford <james.fulford@outlook.com>
2020-05-22 11:05:52 -0400

    Updates

---
+++ precache-manifest.bd9f475b6fce1141d5f610323447c7ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a35b65dfbd658bc86f776cdbf90ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e5d771ddb1043c732d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d23fdf58591e40189ba5",
+    "url": "/static/js/2.2ca916b5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2ca916b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e5d771ddb1043c732d",
+    "url": "/static/js/main.522dd3cb.chunk.js"
+  },
+  {
+    "revision": "6b6444d19fdd6630f484",
+    "url": "/static/js/runtime-main.a22c3945.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
